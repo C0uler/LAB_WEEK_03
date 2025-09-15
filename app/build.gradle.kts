5b988bca932732b4f41825252bae2c18ae06ffc5
C0uler <165772684+C0uler@users.noreply.github.com>
2025-09-15 18:03:02 +0700

    Commit No. 2 – add coffee list & detail fragment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,13 +36,15 @@ android {
 }
 
 dependencies {
-
+    implementation(libs.androidx.fragment.ktx)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation("androidx.fragment:fragment:1.8.9")
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
 }
