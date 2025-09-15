4b106f578006a95f46455a2074af8dcf81fad27f
C0uler <165772684+C0uler@users.noreply.github.com>
2025-09-15 18:24:04 +0700

    Use Dynamic Fragments

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.android.aaptcompiler.parseNavigation
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -40,6 +42,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+//    implementation(libs.androidx.navigation.fragment.ktx)
     implementation("androidx.fragment:fragment:1.8.9")
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
